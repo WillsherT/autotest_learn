9c046438887871ea6e1ad94892e9dd2ed4b1d4d1
Denis Afanasev <da.afanasev1@tensor.ru>
2023-06-09 10:50:58 +0300

    выполнено 11 дз

---
+++ hw11/task_2.py
@@ -0,0 +1,72 @@
+# Авторизоваться на сайте https://fix-online.sbis.ru/
+# Перейти в реестр Контакты
+# Отправить сообщение самому себе
+# Убедиться, что сообщение появилось в реестре
+# Удалить это сообщение и убедиться, что удалили
+# Для сдачи задания пришлите код и запись с экрана прохождения теста
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from time import sleep
+
+browser = webdriver.Chrome()
+login = 'AuthTest00'
+password = 'AuthTest00AuthTest00'
+person_name = 'Гамов Петр'
+sbis_link = 'https://fix-online.sbis.ru/'
+message = 'Приветствую себя'
+
+try:
+    browser.maximize_window()
+    browser.get(sbis_link)
+    sleep(2)
+    login_input = browser.find_element(By.CSS_SELECTOR, "[name='Login']")
+    login_input.send_keys(login)
+    next_btns = browser.find_elements(By.CSS_SELECTOR, ".auth-AdaptiveLoginForm__loginButtonImage")
+    next_btns[0].click()
+    sleep(1)
+    password_input = browser.find_element(By.CSS_SELECTOR, "[name='Password']")
+    password_input.send_keys(password)
+    next_btns[1].click()
+    sleep(6)
+    accordion = browser.find_elements(By.CSS_SELECTOR, ".NavigationPanels-Accordion__title")
+    for title in range(len(accordion)):
+        if accordion[title].text == "Контакты":
+            contacts = accordion[title]
+            contacts.click()
+            break
+    sleep(1)
+    contacts = browser.find_element(By.CSS_SELECTOR, ".NavigationPanels-SubMenu__headTitle")
+    contacts.click()
+    sleep(2)
+    new_message = browser.find_element(By.CSS_SELECTOR, ".icon-RoundPlus")
+    new_message.click()
+    sleep(1)
+    search = browser.find_element(By.CSS_SELECTOR, ".controls-StackTemplate__top-area-content "
+                                                   ".controls-Search__nativeField_caretEmpty")
+    search.send_keys(person_name)
+    sleep(1)
+    find_person = browser.find_element(By.CSS_SELECTOR, ".msg-addressee-item [title='Гамов Петр']")
+    find_person.click()
+    sleep(1)
+    msg_box = browser.find_element(By.CSS_SELECTOR, "[role='textbox']")
+    msg_box.send_keys(message)
+    sleep(1)
+    send_btn = browser.find_element(By.CSS_SELECTOR, ".icon-BtArrow")
+    send_btn.click()
+    sleep(2)
+    sent_messages = browser.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item p')
+    if sent_messages[0].text != message:
+        raise Exception('Отправленное сообщение отсутствует в реестре!')
+    action_chain = ActionChains(browser)
+    action_chain.move_to_element(sent_messages[0])
+    action_chain.perform()
+    delete_btn = browser.find_element(By.CSS_SELECTOR, ".controls-icon_style-danger")
+    delete_btn.click()
+    sleep(1)
+    if browser.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item p')[0] == message:
+        raise Exception('Сообщение не удалено!')
+    sleep(2)
+finally:
+    browser.quit()
---
+++ hw11/task_with_star.py
@@ -0,0 +1,48 @@
+# Перейти на  https://sbis.ru/
+# В Footer'e найти "Скачать СБИС"
+# Перейти по ней
+# Скачать СБИС Плагин для вашей ОС в папку с данным тестом
+# Убедиться, что плагин скачался
+# Вывести на печать размер скачанного файла в мегабайтах
+# Для сдачи задания пришлите код и запись с экрана прохождения теста
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pathlib
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+
+
+path = pathlib.Path.cwd()
+
+options = Options()
+options.add_experimental_option("prefs", {'download.default_directory': str(path), 'safebrowsing.enabled': 'false'})
+
+sbis_link = 'https://sbis.ru/'
+
+browser = webdriver.Chrome(options=options)
+
+try:
+    browser.maximize_window()
+    browser.get(sbis_link)
+    sleep(3)
+    footer = browser.find_element(By.CSS_SELECTOR, ".sbisru-Footer__container")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", footer)
+    footer_links = browser.find_elements(By.CSS_SELECTOR, ".sbisru-Footer__link")
+    sleep(2)
+    for link in range(len(footer_links)):
+        if footer_links[link].text == 'Скачать СБИС':
+            footer_links[link].click()
+            break
+    sleep(3)
+    plugin = browser.find_element(By.CSS_SELECTOR, '[data-id="plugin"]')
+    plugin.click()
+    sleep(2)
+    download_links = browser.find_elements(By.CSS_SELECTOR, '[data-for="plugin"] .sbis_ru-DownloadNew-loadLink')
+    download_links[0].click()
+    sleep(3)
+    for file in path.glob('sbisplugin-setup-web.exe'):
+        print(f'Размер скачанного файла: {round(int(file.stat().st_size) / 1048576, 2)} мб')
+finally:
+    browser.quit()
